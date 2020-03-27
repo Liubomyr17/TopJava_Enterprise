b18b3c6f4c3a1cdc5ca611957f6548b4e5ca48d3
Liubomyr Izdepskyi <lizdepskij@gmail.com>
2020-03-27 12:37:12 +0200

    1_4(1) HW1 patch

--- src/main/java/ru/javawebinar/topjava/web/UserServlet.java
+++ src/main/java/ru/javawebinar/topjava/web/UserServlet.java
@@ -1,6 +1,15 @@
 package ru.javawebinar.topjava.web;
 
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-public class UserServlet {
+public class UserServlet extends HttpServlet {
+
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        request.getRequestDispatcher("/user.jsp").forward(request, response);
+    }
 }
