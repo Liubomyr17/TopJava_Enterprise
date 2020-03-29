b7d86f5224d90e4d7558cac08fafaeaea1cf0391
Liubomyr Izdepskyi <lizdepskij@gmail.com>
2020-03-29 13:28:04 +0300

    2_7_constructor_injection

--- src/main/java/ru/javawebinar/topjava/service/UserService.java
+++ src/main/java/ru/javawebinar/topjava/service/UserService.java
@@ -16,6 +16,7 @@ public class UserService {
 
     private final UserRepository repository;
 
+    @Autowired
     public UserService(UserRepository repository) {
         this.repository = repository;
     }
