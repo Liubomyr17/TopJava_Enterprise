80e6b9eaf683c35cc5460e6c32db6c2865aaf29b
Liubomyr Izdepskyi <lizdepskij@gmail.com>
2020-03-31 19:59:01 +0300

    3_07_add_junit

--- src/main/java/ru/javawebinar/topjava/repository/inmemory/InMemoryMealRepository.java
+++ src/main/java/ru/javawebinar/topjava/repository/inmemory/InMemoryMealRepository.java
@@ -7,7 +7,6 @@ import ru.javawebinar.topjava.model.Meal;
 import ru.javawebinar.topjava.repository.MealRepository;
 import ru.javawebinar.topjava.util.MealsUtil;
 import ru.javawebinar.topjava.util.Util;
-
 import javax.annotation.PostConstruct;
 import javax.annotation.PreDestroy;
 import java.time.LocalDateTime;
@@ -20,62 +19,50 @@ import java.util.concurrent.ConcurrentHashMap;
 import java.util.function.Predicate;
 import java.util.stream.Collectors;
 
-import static ru.javawebinar.topjava.repository.inmemory.InMemoryUserRepository.ADMIN_ID;
-import static ru.javawebinar.topjava.repository.inmemory.InMemoryUserRepository.USER_ID;
+import static ru.javawebinar.topjava.UserTestData.ADMIN_ID;
+import static ru.javawebinar.topjava.UserTestData.USER_ID;
 
 @Repository
 public class InMemoryMealRepository implements MealRepository {
     private static final Logger log = LoggerFactory.getLogger(InMemoryMealRepository.class);
-
     // Map  userId -> mealRepository
     private Map<Integer, InMemoryBaseRepository<Meal>> usersMealsMap = new ConcurrentHashMap<>();
-
     {
         MealsUtil.MEALS.forEach(meal -> save(meal, USER_ID));
-
         save(new Meal(LocalDateTime.of(2015, Month.JUNE, 1, 14, 0), "Админ ланч", 510), ADMIN_ID);
         save(new Meal(LocalDateTime.of(2015, Month.JUNE, 1, 21, 0), "Админ ужин", 1500), ADMIN_ID);
     }
-
-
     @Override
     public Meal save(Meal meal, int userId) {
         InMemoryBaseRepository<Meal> meals = usersMealsMap.computeIfAbsent(userId, uid -> new InMemoryBaseRepository<>());
         return meals.save(meal);
     }
-
     @PostConstruct
     public void postConstruct() {
         log.info("+++ PostConstruct");
     }
-
     @PreDestroy
     public void preDestroy() {
         log.info("+++ PreDestroy");
     }
-
     @Override
     public boolean delete(int id, int userId) {
         InMemoryBaseRepository<Meal> meals = usersMealsMap.get(userId);
         return meals != null && meals.delete(id);
     }
-
     @Override
     public Meal get(int id, int userId) {
         InMemoryBaseRepository<Meal> meals = usersMealsMap.get(userId);
         return meals == null ? null : meals.get(id);
     }
-
     @Override
     public List<Meal> getAll(int userId) {
         return getAllFiltered(userId, meal -> true);
     }
-
     @Override
     public List<Meal> getBetween(LocalDateTime startDateTime, LocalDateTime endDateTime, int userId) {
         return getAllFiltered(userId, meal -> Util.isBetweenInclusive(meal.getDateTime(), startDateTime, endDateTime));
     }
-
     private List<Meal> getAllFiltered(int userId, Predicate<Meal> filter) {
         InMemoryBaseRepository<Meal> meals = usersMealsMap.get(userId);
         return meals == null ? Collections.emptyList() :
