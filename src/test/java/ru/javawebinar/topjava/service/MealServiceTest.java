4316605396dd381e5230db186b68ecb50ff02ca8
Liubomyr Izdepskyi <lizdepskij@gmail.com>
2020-04-06 12:50:09 +0300

    5_6

--- src/test/java/ru/javawebinar/topjava/service/MealServiceTest.java
+++ src/test/java/ru/javawebinar/topjava/service/MealServiceTest.java
@@ -1,5 +1,7 @@
 package ru.javawebinar.topjava.service;
 
+import org.springframework.test.context.ActiveProfiles;
+import ru.javawebinar.topjava.Profiles;
 import org.junit.AfterClass;
 import org.junit.Rule;
 import org.junit.Test;
@@ -32,6 +34,7 @@ import static ru.javawebinar.topjava.UserTestData.USER_ID;
 })
 @RunWith(SpringRunner.class)
 @Sql(scripts = "classpath:db/populateDB.sql", config = @SqlConfig(encoding = "UTF-8"))
+@ActiveProfiles(Profiles.ACTIVE_DB)
 public class MealServiceTest {
 
     private static final Logger log = getLogger("result");
