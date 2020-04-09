139b97a466f640e9be30e75e9d6008d74b4792fd
Liubomyr Izdepskyi <lizdepskij@gmail.com>
2020-04-09 20:30:35 +0300

    6_10

--- src/main/java/ru/javawebinar/topjava/model/Meal.java
+++ src/main/java/ru/javawebinar/topjava/model/Meal.java
@@ -3,7 +3,8 @@ package ru.javawebinar.topjava.model;
 import javax.persistence.FetchType;
 import javax.persistence.ManyToOne;
 import org.hibernate.validator.constraints.Range;
-
+import org.hibernate.annotations.OnDelete;
+import org.hibernate.annotations.OnDeleteAction;
 import javax.persistence.*;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
@@ -42,6 +43,7 @@ public class Meal extends AbstractBaseEntity {
 
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "user_id", nullable = false)
+    @OnDelete(action = OnDeleteAction.CASCADE)
     @NotNull
     private User user;
 
