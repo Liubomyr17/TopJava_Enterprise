2d4a0c53c8f64f2eeeeca754a9e33eae7462c431
Liubomyr Izdepskyi <lizdepskij@gmail.com>
2020-04-16 14:03:40 +0300

    10_14

--- src/main/java/ru/javawebinar/topjava/web/json/JsonUtil.java
+++ src/main/java/ru/javawebinar/topjava/web/json/JsonUtil.java
@@ -1,6 +1,9 @@
 package ru.javawebinar.topjava.web.json;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.core.type.TypeReference;
+import java.util.Map;
+
 import com.fasterxml.jackson.databind.ObjectReader;
 
 import java.io.IOException;
@@ -31,6 +34,15 @@ public class JsonUtil {
         } catch (JsonProcessingException e) {
             throw new IllegalArgumentException("Invalid write to JSON:\n'" + obj + "'", e);
         }
+    }
+    public static <T> String writeAdditionProps(T obj, String addName, Object addValue) {
+        return writeAdditionProps(obj, Map.of(addName, addValue));
+    }
+
+    public static <T> String writeAdditionProps(T obj, Map<String, Object> addProps) {
+        Map<String, Object> map = getMapper().convertValue(obj, new TypeReference<Map<String, Object>>() {});
+        map.putAll(addProps);
+        return writeValue(map);
     }
 
 }
