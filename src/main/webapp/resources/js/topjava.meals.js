aa1c21a3dbdd30f1cb64d17f0b13820d40657ca0
Liubomyr Izdepskyi <lizdepskij@gmail.com>
2020-04-16 17:45:16 +0300

    11_7

--- src/main/webapp/resources/js/topjava.meals.js
+++ src/main/webapp/resources/js/topjava.meals.js
@@ -64,6 +64,9 @@ $(function () {
         updateTable: updateFilteredTable
     });
 
+    $.datetimepicker.setLocale(localeCode);
+
+
 //  http://xdsoft.net/jqplugins/datetimepicker/
     var startDate = $('#startDate');
     var endDate = $('#endDate');
