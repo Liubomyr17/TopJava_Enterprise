556adb81ed7167f5acff6fdeca4b4e1774c5c9bc
Liubomyr Izdepskyi <lizdepskij@gmail.com>
2020-04-16 12:46:52 +0300

    10_13

--- src/main/java/ru/javawebinar/topjava/service/UserService.java
+++ src/main/java/ru/javawebinar/topjava/service/UserService.java
@@ -1,12 +1,14 @@
 package ru.javawebinar.topjava.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.cache.annotation.CacheEvict;
 import org.springframework.cache.annotation.Cacheable;
 import org.springframework.context.annotation.Scope;
 import org.springframework.context.annotation.ScopedProxyMode;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.util.Assert;
@@ -15,53 +17,66 @@ import ru.javawebinar.topjava.model.User;
 import ru.javawebinar.topjava.repository.UserRepository;
 import ru.javawebinar.topjava.to.UserTo;
 import ru.javawebinar.topjava.util.UserUtil;
+
 import java.util.List;
+
+import static ru.javawebinar.topjava.util.UserUtil.prepareToSave;
 import static ru.javawebinar.topjava.util.ValidationUtil.checkNotFound;
 import static ru.javawebinar.topjava.util.ValidationUtil.checkNotFoundWithId;
-
 
 @Service("userService")
 @Scope(proxyMode = ScopedProxyMode.TARGET_CLASS)
 public class UserService implements UserDetailsService {
 
     private final UserRepository repository;
+    private final PasswordEncoder passwordEncoder;
 
     @Autowired
-    public UserService(UserRepository repository) {
+    public UserService(UserRepository repository, PasswordEncoder passwordEncoder) {
         this.repository = repository;
+        this.passwordEncoder = passwordEncoder;
     }
+
+
     @CacheEvict(value = "users", allEntries = true)
     public User create(User user) {
         Assert.notNull(user, "user must not be null");
-        return repository.save(user);
+        return prepareAndSave(user);
     }
+
     @CacheEvict(value = "users", allEntries = true)
     public void delete(int id) {
         checkNotFoundWithId(repository.delete(id), id);
     }
+
     public User get(int id) {
         return checkNotFoundWithId(repository.get(id), id);
     }
+
     public User getByEmail(String email) {
         Assert.notNull(email, "email must not be null");
         return checkNotFound(repository.getByEmail(email), "email=" + email);
     }
+
     @Cacheable("users")
     public List<User> getAll() {
         return repository.getAll();
     }
+
     @CacheEvict(value = "users", allEntries = true)
     public void update(User user) {
         Assert.notNull(user, "user must not be null");
 //      checkNotFoundWithId : check works only for JDBC, disabled
-        repository.save(user);
+        prepareAndSave(user);
     }
+
     @CacheEvict(value = "users", allEntries = true)
     @Transactional
     public void update(UserTo userTo) {
         User user = get(userTo.id());
-        repository.save(UserUtil.updateFromTo(user, userTo));
+        prepareAndSave(UserUtil.updateFromTo(user, userTo));
     }
+
     @CacheEvict(value = "users", allEntries = true)
     @Transactional
     public void enable(int id, boolean enabled) {
@@ -77,6 +92,10 @@ public class UserService implements UserDetailsService {
             throw new UsernameNotFoundException("User " + email + " is not found");
         }
         return new AuthorizedUser(user);
+    }
+
+    private User prepareAndSave(User user) {
+        return repository.save(prepareToSave(user, passwordEncoder));
     }
 
     public User getWithMeals(int id) {
--- src/main/java/ru/javawebinar/topjava/util/exception/ErrorInfo.java
+++ src/main/java/ru/javawebinar/topjava/util/exception/ErrorInfo.java
@@ -1,16 +1,14 @@
 package ru.javawebinar.topjava.util.exception;
 
-import javax.lang.model.type.ErrorType;
 
 public class ErrorInfo {
+    private final String url;
+    private final ErrorType type;
+    private final String detail;
 
-        private final String url;
-        private final ErrorType type;
-        private final String detail;
-
-        public ErrorInfo(CharSequence url, ErrorType type, String detail) {
-            this.url = url.toString();
-            this.type = type;
-            this.detail = detail;
-        }
+    public ErrorInfo(CharSequence url, ErrorType type, String detail) {
+        this.url = url.toString();
+        this.type = type;
+        this.detail = detail;
     }
+}
